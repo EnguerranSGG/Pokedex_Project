049e91f41b0511b3fb6f154bd7dce1b856750f26
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2024-06-25 18:41:21 +0200

    Fetching data from pokemon's API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+
 import { RouterOutlet } from '@angular/router';
 
+import { PokelistComponent } from './pokelist/pokelist.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PokelistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'pokedex_project';
+  title = 'Pokédex';
 }
