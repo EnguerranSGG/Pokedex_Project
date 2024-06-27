370a02a595ab8f9e76a8e8db2cfc23cba9b53d71
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2024-06-27 23:22:49 +0200

    Create 'services .ts' to function converting data

--- src/app/pokelist/pokelist.component.ts
+++ src/app/pokelist/pokelist.component.ts
@@ -4,6 +4,8 @@ import { Pokemon } from '../../interfaces/pokemon';
 
 import { NgFor, CommonModule} from '@angular/common';
 
+import { renderProperGeneration, renderPokedexIndex, renderUsableHeight, renderUsableWeight, getTypeIcon } from '../services/services';
+
 @Component({
   selector: 'app-pokelist',
   standalone: true,
@@ -34,11 +36,6 @@ export class PokelistComponent implements OnInit {
     console.log(this.PokemonData);
   }
 
-  capitalizeFirstLetter(str: string): string {
-    if (!str) return str;
-    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-  }
-
 }
 
 async function GetPokemonData(arg: number) : Promise<Pokemon> {
@@ -51,90 +48,6 @@ async function GetPokemonData(arg: number) : Promise<Pokemon> {
     (await fetch(pokemonUrl)).json(),
     (await fetch(pokemonSpeciesUrl)).json()
   ])
-
-  function renderPokedexIndex(pokeIndex: number) : string {
-    if (pokeIndex < 10) {
-      return `#00${pokeIndex}`;
-    } else if (pokeIndex < 100) {
-      return `#0${pokeIndex}`;
-    } else {
-      return `#${pokeIndex}`;
-    }
-  }
-
-  function renderUsableHeight(height: number) : number {
-    return height * 10 / 100;
-  }
-
-  function renderUsableWeight(height: number) : number {
-    return height * 10 / 100;
-  }
-
-  function getTypeIcon(type: string) : string {
-    switch (type) {
-      case 'normal':
-        return 'https://github.com/EnguerranSGG/Pokedex_Project/blob/main/src/images/type_normal.png?raw=true';
-      case 'fighting':
-        return 'https://github.com/EnguerranSGG/Pokedex_Project/blob/main/src/images/type_combat.png?raw=true';
-      case 'flying':
-        return 'https://github.com/EnguerranSGG/Pokedex_Project/blob/main/src/images/type_vol.png?raw=true';
-      case 'poison':
-        return 'https://github.com/EnguerranSGG/Pokedex_Project/blob/main/src/images/type_poison.png?raw=true';
-      case 'ground':
-        return 'https://github.com/EnguerranSGG/Pokedex_Project/blob/main/src/images/type_sol.png?raw=true';
-      case 'rock':
-        return 'https://github.com/EnguerranSGG/Pokedex_Project/blob/main/src/images/type_roche.png?raw=true';
-      case 'bug':
-        return 'https://github.com/EnguerranSGG/Pokedex_Project/blob/main/src/images/type_insecte.png?raw=true';
-      case 'ghost':
-        return 'https://github.com/EnguerranSGG/Pokedex_Project/blob/main/src/images/type_spectre.png?raw=true';
-      case 'steel':
-        return 'https://github.com/EnguerranSGG/Pokedex_Project/blob/main/src/images/type_acier.png?raw=true';
-      case 'fire':
-        return 'https://github.com/EnguerranSGG/Pokedex_Project/blob/main/src/images/type_feu.png?raw=true';
-      case 'water':
-        return 'https://github.com/EnguerranSGG/Pokedex_Project/blob/main/src/images/type_eau.png?raw=true';
-      case 'grass':
-        return 'https://github.com/EnguerranSGG/Pokedex_Project/blob/main/src/images/type_plante.png?raw=true';
-      case 'electric':
-        return 'https://github.com/EnguerranSGG/Pokedex_Project/blob/main/src/images/type_electrique.png?raw=true';
-      case 'psychic':
-        return 'https://github.com/EnguerranSGG/Pokedex_Project/blob/main/src/images/type_psy.png?raw=true';
-      case 'ice':
-        return 'https://github.com/EnguerranSGG/Pokedex_Project/blob/main/src/images/type_glace.png?raw=true';
-      case 'dragon':
-        return 'https://github.com/EnguerranSGG/Pokedex_Project/blob/main/src/images/type_dragon.png?raw=true';
-      case 'dark':
-        return 'https://github.com/EnguerranSGG/Pokedex_Project/blob/main/src/images/type_tenebres.png?raw=true';
-      case 'fairy':
-        return 'https://github.com/EnguerranSGG/Pokedex_Project/blob/main/src/images/type_fee.png?raw=true';
-      default:
-        return '';
-    }
-  }
-
-  function renderProperGeneration(generation: string) : string {
-    switch (generation) {
-      case 'generation-i':
-        return 'première';
-      case 'generation-ii':
-        return 'deuxième';
-      case 'generation-iii':
-        return 'troisième';
-      case 'generation-iv':
-        return 'quatrième';
-      case 'generation-v':
-        return 'cinquième';
-      case 'generation-vi':
-        return 'sixième';
-      case 'generation-vii':
-        return 'septième';
-      case 'generation-viii':
-        return 'huitième';
-      default:
-        return '';
-    }
-  }
 
   return {
     pokeIndex: pokemonData.id,
