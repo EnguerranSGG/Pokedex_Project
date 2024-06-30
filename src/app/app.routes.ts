69865d19ad8e89f9d938effdce0c7e4af8d5f746
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2024-06-30 19:41:55 +0200

    delete useless route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,6 @@ import { PokelistComponent } from './pokelist/pokelist.component';
 import { PokemonFocusComponent } from './pokefocus/pokefocus.component';
 
 export const routes: Routes = [
-    {path: "pokedex", component: PokelistComponent},
+    {path: "", component: PokelistComponent},
     {path: "pokedex/:pokeIndex", component: PokemonFocusComponent}
 ];
