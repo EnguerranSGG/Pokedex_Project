b80ce9e2dcf3cf41604da2f6c44d98e7487aa0f9
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2024-06-30 19:52:47 +0200

    Activate route to home

--- src/app/pokeheader/pokeheader.component.ts
+++ src/app/pokeheader/pokeheader.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pokeheader',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pokeheader.component.html',
   styleUrl: './pokeheader.component.scss'
 })
